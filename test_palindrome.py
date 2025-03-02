077d5429376108284bfe2d71d794461b49c74229
Gurinder Singh <92890724+1stgoal@users.noreply.github.com>
2025-03-02 06:50:27 -0500

    fixed the comment

--- test_palindrome.py
+++ test_palindrome.py
@@ -6,7 +6,11 @@ import pytest
 from palindrome import is_palindrome
 
 
-# test_not_string raises a ValueError when not str.
+# is_palindrome raises a ValueError when not str.
 def test_not_string():
     with pytest.raises(ValueError):
         is_palindrome(7172)
+
+
+
+
