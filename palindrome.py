e9c05544281b8c210eb54eeb4c5058841d69212f
Gurinder Singh <92890724+1stgoal@users.noreply.github.com>
2025-03-02 06:08:08 -0500

    test_not_string raises a ValueError when not str.

--- palindrome.py
+++ palindrome.py
@@ -1,3 +1,7 @@
-"""
-Validates strings as palindromes.
-"""
+
+# is_palindrome accepts str or else raise ValueError when not str
+def is_palindrome(value: str) -> bool:
+    if not isinstance(value, str):
+        raise ValueError
+    else:
+        return True
--- test_palindrome.py
+++ test_palindrome.py
@@ -1,3 +1,12 @@
 """
 Tests the palindrome module
 """
+
+import pytest
+from palindrome import is_palindrome
+
+
+# test_not_string raises a ValueError when not str.
+def test_not_string():
+    with pytest.raises(ValueError):
+        is_palindrome(7172)
